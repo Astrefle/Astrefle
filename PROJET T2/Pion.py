d0c158cdd7dc625ce35634464ebe0012e97bb3d0
Astrefle <92527338+Astrefle@users.noreply.github.com>
2021-10-21 18:57:14 +0200

    Update Pion.py

--- PROJET T2/Pion.py
+++ PROJET T2/Pion.py
@@ -1,15 +1,33 @@
 import pygame
 pygame.init()
+from Grille import*
 
-class Pion(pygame.sprite.Sprite):
-    def __init__ (self):
+class PION(pygame.sprite.Sprite):
+    def __init__ (self,grille):
         super().__init__
-        self.p1 = pygame.image.load('assets/SPRITE_/Image1-removebg-preview.png')
+        self.p1 = pygame.image.load('assets/SPRITE_/PION.png')
         self.hit_p1= self.p1.get_rect()
-        self.hit_p1.x=500
-        self.hit_p1.y=600  
+        print(self.hit_p1)
+        self.hit_p1.x=310+190
+        self.hit_p1.y=100
+        self.colonne=4
+        self.grille=grille
 
+        
+
+
+        
     def Right(self):
-        self.hit_p1.x +=1
+        self.hit_p1.x +=100
+        self.colonne+=1
+        print (self.colonne)
+        return self.colonne
     def Left(self):
-        self.hit_p1.x -=1
+        self.hit_p1.x -=100
+        self.colonne-=1
+        print (self.colonne)
+        return self.colonne
+    def tombe(self):
+        self.hit_p1.y+=400
+        
+        self.grille.ajout_pion(self.colonne) 
