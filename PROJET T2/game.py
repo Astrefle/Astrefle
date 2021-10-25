fb90779d9ad8390436929876abf23c5d90344824
Astrefle <92527338+Astrefle@users.noreply.github.com>
2021-10-25 13:14:45 +0200

    Update game.py

--- PROJET T2/game.py
+++ PROJET T2/game.py
@@ -7,17 +7,30 @@ from Grille import*
 class Game:
     def __init__(self):
         self.grille=Grille()
-       
+    
         self.l_pion=[]
         
-        self.pion= PION(self.grille)
+        self.pion= PION(self.grille,self.l_pion)
         
         self.pressed ={}
 
         print(self.pressed)
+        
         self.l_pion.append(self.pion)
+    
     def newpion(self):
-        self.pion=PION(self.grille)
-        self.l_pion.append(self.pion)
-        return self.pion
        
+        self.pion=PION(self.grille,self.l_pion)
+        
+        self.l_pion.append(self.pion)
+        
+        if (len(self.l_pion)%2)==0:
+            self.pion.p1=pygame.image.load('assets/SPRITE_/pion/PION2.png')  
+        
+        else:
+            self.pion.p1=pygame.image.load('assets/SPRITE_/pion/PION.png')
+        
+        return self.pion
+    
+    
+           
