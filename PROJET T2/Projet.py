ec6ac03d807ed24e89977f03a6c99e43a2d38911
Astrefle <92527338+Astrefle@users.noreply.github.com>
2021-11-08 18:21:26 +0100

    Update Projet.py

--- PROJET T2/Projet.py
+++ PROJET T2/Projet.py
@@ -6,6 +6,7 @@ pygame.init()
 from Pion import*
 from game import*
 from Titre import*
+from choi import*
 
 
 
@@ -21,6 +22,8 @@ running=True
 bg_=pygame.image.load('assets/BG_/WarmWarmheartedIrishwolfhound-max-1mb.gif')
 
 Titre= TITLE()
+Choix=Choix_perso()
+
 
 
 
@@ -32,17 +35,20 @@ while running:
     screen.blit(Titre.title1,Titre.hit1)
     screen.blit(Titre.title2,Titre.hit2)
     screen.blit(Titre.title3,Titre.hit3)
-#Test affichage Pion
+
+#Test affichage perso
+    screen.blit(Choix.perso_choisis,Choix.perso_choisis_hit)
+    if game.pressed.get(pygame.K_RIGHT):
+        Choix.Right()
+    elif game.pressed.get(pygame.K_LEFT):
+        Choix.Left()  
+    
+        
+   
     for i in game.l_pion:
         screen.blit(i.p1,i.hit_p1)
-        
-
-
-    
-
-    
-
-#Déplacement du perso    
+            
+    #Déplacement du pion   
     if game.pressed.get(pygame.K_RIGHT) and game.pion.hit_p1.x+game.pion.hit_p1.width<880:
         game.pion.Right()
     elif game.pressed.get(pygame.K_LEFT) and game.pion.hit_p1.x>200:
@@ -50,13 +56,23 @@ while running:
     elif game.pressed.get(pygame.K_RETURN) and game.pion.hit_p1.y <700:
         game.pion.tombe()
         game.newpion()
+        
+    pygame.display.flip()            
+
+
+
     
-    pygame.display.flip()
+#Test affichage Pion
+   
     
+  
+  
+  
    #def des touches du jeu 
     game.pressed[pygame.K_RIGHT] = False
     game.pressed[pygame.K_LEFT] = False
     game.pressed[pygame.K_RETURN] = False
+    
     
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
@@ -65,7 +81,4 @@ while running:
         
         if i.type == pygame.KEYUP:
             game.pressed[i.key] = True
-
-    
-            
             
