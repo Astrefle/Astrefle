086d2e8cf5bb6c939496271609e1f050180f9c0e
Astrefle <92527338+Astrefle@users.noreply.github.com>
2021-11-10 22:47:37 +0100

    Update Grille.py

--- PROJET T2/Grille.py
+++ PROJET T2/Grille.py
@@ -1,50 +1,32 @@
+from Colonne import*
+from Pion import*
 import pygame
 pygame.init()
-from Pion import*
-from Colonne import*
-
-    
-
 
 
 class Grille:
     def __init__(self):
-        self.stage=5
-        
-        self.grille=[]
-        
+        self.stage = 5
+
+        self.grille = []
+
         for line in range(7):
             self.grille.append(Colonne())
-            
-    
-    def est_vide(self,colonne):
+
+    def est_vide(self, colonne):
         return self.grille[colonne-1].est_vide()
-        
-    
-    def taille(self,colonne):
-        
-        print (self.grille[colonne-1].taille())   
-        return (self.grille[colonne-1].taille())   
-        
-   
-    def ajout_pion(self,colonne,p):
-        if not self.taille(colonne) >5:
-  
+
+    def taille(self, colonne):
+
+        print(self.grille[colonne-1].taille())
+        return (self.grille[colonne-1].taille())
+
+    def ajout_pion(self, colonne, p):
+        if not self.taille(colonne) > 5:
+
             self.grille[colonne-1].empiler(p)
-               
+
         print(self.grille)
-        
-    """def puissance4(self)"""
 
-
-
-        
-     
-
-
-
-
-
-
-
-        
+        if self.grille[colonne-1] == 4:
+            print('puissance 4')
