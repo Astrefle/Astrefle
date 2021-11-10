8790adb99be4e08362786d862adfb28fec9e570a
Astrefle <92527338+Astrefle@users.noreply.github.com>
2021-11-10 22:45:04 +0100

    Update bouton.py

--- PROJET T2/bouton.py
+++ PROJET T2/bouton.py
@@ -1,48 +1,74 @@
+from choi2 import*
+from Titre import*
+from game import*
+from choi import*
 import pygame
 from pygame.version import PygameVersion
+
+from choi2 import Choix_perso2
 pygame.init()
-from choi import*
-from game import*
-from Titre import*
 
 pygame.display.set_caption("Brawl 4 Monsters")
-screen=pygame.display.set_mode((600,700))
+screen = pygame.display.set_mode((600, 700))
 
-running=True
+run = True
+jeu_start = 0
+suivant = 1
+bg_ = pygame.image.load('assets/BG_/WarmWarmheartedIrishwolfhound-max-1mb.gif')
+Titre = TITLE()
+Choix1 = Choix_perso()
+Choix2 = Choix_perso2()
+Choix2.perso_choisis_hit.y += 300
+pressed = {}
 
-bg_=pygame.image.load('assets/BG_/WarmWarmheartedIrishwolfhound-max-1mb.gif')
-Titre=TITLE()
-Choix=Choix_perso()
-game=Game()
-while running:
-#fond d'ecran
-    screen.blit(bg_,(0,0))
-#Titre du jeu
-    screen.blit(Titre.title1,Titre.hit1)
-    screen.blit(Titre.title2,Titre.hit2)
-    screen.blit(Titre.title3,Titre.hit3)
-    screen.blit(Choix.perso_choisis,Choix.perso_choisis_hit)
+while run:
+    # fond d'ecran
+    screen.blit(bg_, (0, 0))
+# Titre du jeu
+    screen.blit(Titre.title1, Titre.hit1)
+    screen.blit(Titre.title2, Titre.hit2)
+    screen.blit(Titre.title3, Titre.hit3)
+# Perso 1 et 2
+    screen.blit(Choix1.perso_choisis, Choix1.perso_choisis_hit)
+    screen.blit(Choix2.perso_choisis, Choix2.perso_choisis_hit)
     pygame.display.flip()
 
-    game.pressed[pygame.K_RIGHT] = False
-    game.pressed[pygame.K_LEFT] = False
-    game.pressed[pygame.K_RETURN] = False
-
-
+    pressed[pygame.K_RIGHT] = False
+    pressed[pygame.K_LEFT] = False
+    pressed[pygame.K_RETURN] = False
 
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
-            running=False
+
+            run = False
             pygame.quit()
 
         if i.type == pygame.KEYUP:
-            game.pressed[i.key] = True
+            pressed[i.key] = True
 
-    if game.pressed.get(pygame.K_RIGHT):
-        Choix.Right()
-    elif game.pressed.get(pygame.K_LEFT):
-        Choix.Left()
+        if not suivant == 0:
+            if pressed.get(pygame.K_RIGHT):
+                Choix1.Right()
+            elif pressed.get(pygame.K_LEFT):
+                Choix1.Left()
+            if pressed.get(pygame.K_RETURN):
+                jeu_start += 1
+                joueur1 = Choix1.perso_choisis
+                Choix1.perso_choisis_hit.y += 300
+                Choix2.perso_choisis_hit.y -= 300
+                suivant = 0
 
-    elif game.pressed.get(pygame.K_RETURN):
-        running=False
-        pygame.quit()
+        if suivant == 0:
+
+            if pressed.get(pygame.K_RIGHT):
+                Choix2.Right()
+            elif pressed.get(pygame.K_LEFT):
+                Choix2.Left()
+            if pressed.get(pygame.K_RETURN):
+                jeu_start += 1
+                joueur2 = Choix2.perso_choisis
+
+        if jeu_start == 3:
+            running = True
+            run = False
+            pygame.quit()
