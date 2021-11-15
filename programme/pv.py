d93bbc56bc85bd8d50d1689e92e30dc54277e81d
Astrefle <92527338+Astrefle@users.noreply.github.com>
2021-11-15 13:40:54 +0100

    Update pv.py

--- programme/pv.py
+++ programme/pv.py
@@ -9,12 +9,18 @@ class PV:
     def __init__(self):
         self.l_pv=['pv1.png','pv2.png','pv3.png','pv4.png',]
         self.num=0
+        self.num1=0
+        self.num2=0
         self.pv=self.l_pv[self.num]
         self.j1=joueur1_hit
 
-    def degat(self):
-        self.num+=1
-        self.pv=self.l_pv[self.num]
+    def degat1(self):
+        self.num1+=1
+        self.pv=self.l_pv[self.num1]
+    
+    def degat2(self):
+        self.num2+=1
+        self.pv=self.l_pv[self.num2]
        
         
 
